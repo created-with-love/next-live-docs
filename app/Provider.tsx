bda0c2c2bba5fe94ce0222a04fa3aff5c4856823
Volodymyr Ivanchenko <vladymyrivanchenko3@gmail.com>
2024-09-29 17:43:13 +0300

    add mentions functionality in the room comments section

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -5,19 +5,40 @@ import {
   ClientSideSuspense,
 } from '@liveblocks/react/suspense';
 import Loader from '@/components/Loader';
-import { getClerkUsers } from '@/lib/actions/user.action';
+import { getClerkUsers, getDocumentUsers } from '@/lib/actions/user.action';
+import { useUser } from '@clerk/nextjs';
 
 const Provider = ({ children }: { children: ReactNode }) => {
+  const { user: clerkUser } = useUser();
+
   const resolveUsers = async ({ userIds }: { userIds: string[] }) => {
     const users = await getClerkUsers({ userIds });
 
     return users;
   };
 
+  const resolveMentionSuggestions = async ({
+    text,
+    roomId,
+  }: {
+    text: string;
+    roomId: string;
+  }) => {
+    const roomUsers = await getDocumentUsers({
+      roomId,
+      // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+      currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+      text,
+    });
+
+    return roomUsers;
+  };
+
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
       resolveUsers={resolveUsers}
+      resolveMentionSuggestions={resolveMentionSuggestions}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
     </LiveblocksProvider>
